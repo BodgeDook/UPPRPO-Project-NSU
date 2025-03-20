e8cf40892c5b5a06ced1a0d26bc1c9456dbfcb27
iceicebabyaf <144206115+iceicebabyaf@users.noreply.github.com>
2025-03-20 16:01:38 +0700

    Update main.py
    print -> raise erros to get a error codes

--- SQL/users/main.py
+++ SQL/users/main.py
@@ -2,14 +2,15 @@ import json
 
 from decimal import Decimal 
 from fastapi import FastAPI, Query
+from fastapi import HTTPException
 import uvicorn
 import psycopg2
 
 
 DB_PARAMS = {
-    "dbname": "",
-    "user": "",
-    "password": "",  
+    "dbname": "upprpo_users",
+    "user": "postgres",
+    "password": "1234",  
     "host": "localhost",
     "port": "5432"
 }
@@ -21,38 +22,20 @@ def connect_to_db():
         cur = conn.cursor()
         return conn, cur
     except Exception as e:
-        print(f"Error with connection to db: {e}")
-        return None, None
+        raise HTTPException(status_code=500, detail="DB connection failed")
 
-
-def check_email_exists(email):
-    try:
-        conn, cur = connect_to_db()
-        if conn is None or cur is None:
-            return False
-
-        cur.execute("SELECT COUNT(*) FROM parsed_data WHERE email = %s;", (email,))
-        result = cur.fetchone()
-
-        conn.close()
-
-        return result[0] > 0
-    except Exception as e:
-        print(f"Exception checking email: {e}")
-        return False
-    
 
 def save_to_db(email: str, password: str):
     try:
         conn, cur = connect_to_db()
         if conn is None or cur is None:
-            return {"error": "DB didn't connected"}
+            raise HTTPException(status_code=500, detail="DB connection failed")
 
         cur.execute("SELECT COUNT(*) FROM users WHERE email = %s;", (email,))
         result = cur.fetchone()
 
         if result[0] > 0:
-            return {"error": "Email already exists"}
+            raise HTTPException(status_code=409, detail="Email already exists")
         
 
         cur.execute(
@@ -69,15 +52,14 @@ def save_to_db(email: str, password: str):
         return {"status": "success", "email": email}
 
     except Exception as e:
-        print(f"Exception with saving bd: {e}")
-        return {"error": str(e)}
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
 
 def get_data_from_db():
 
     try:
         conn, cur = connect_to_db()
         if conn is None or cur is None:
-            return {"error": "DB didn't connected"}
+            raise HTTPException(status_code=500, detail="DB connection failed")
         
         cur.execute("SELECT * FROM users;")  
         rows = cur.fetchall()
@@ -91,20 +73,19 @@ def get_data_from_db():
 
         return data
     except Exception as e:
-        print(f"Exception with reading bd: {e}")
-        return {"error": str(e)}
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
 
 def update_balance(email: str, amount: float):
     try:
         conn, cur = connect_to_db()
         if conn is None or cur is None:
-            return {"error": "DB didn't connect"}
+            raise HTTPException(status_code=500, detail="DB connection failed")
 
         cur.execute("SELECT COUNT(*) FROM users WHERE email = %s;", (email,))
         result = cur.fetchone()
 
         if result[0] == 0:
-            return {"error": "Email not found"}
+            raise HTTPException(status_code=401, detail="Invalid email")
 
 
         cur.execute("SELECT balance FROM users WHERE email = %s;", (email,))
@@ -127,30 +108,30 @@ def update_balance(email: str, amount: float):
         return {"status": "success", "email": email, "new_balance": float(new_balance)}
 
     except Exception as e:
-        print(f"Exception with updating balance: {e}")
-        return {"error": str(e)}
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
 
 def login(email: str, password: str):
     try:
         conn, cur = connect_to_db()
         if conn is None or cur is None:
-            return {"error": "DB didn't connect"}
+            raise HTTPException(status_code=500, detail="DB connection failed")
 
-        cur.execute("SELECT email, balance FROM users WHERE email = %s AND password =%s;", (email,password))
+        cur.execute("SELECT email, balance FROM users WHERE email = %s AND password = %s;", (email, password))
         result = cur.fetchone()
 
         if result is None:
-            return {"error": "Invalid email or password!"}
+            raise HTTPException(status_code=401, detail="Invalid email or password")
 
         user_email, user_balance = result
+
         cur.execute(
-                    """
-                    UPDATE users
-                    SET is_logged_in = TRUE
-                    WHERE email = %s;
-                    """,
-                    (email,)
-                )
+            """
+            UPDATE users
+            SET is_logged_in = TRUE
+            WHERE email = %s;
+            """,
+            (email,)
+        )
 
         conn.commit()
         cur.close()
@@ -158,22 +139,23 @@ def login(email: str, password: str):
         return {"email": user_email, "balance": user_balance}
 
     except Exception as e:
-        print(f"Exception with updating balance: {e}")
-        return {"error": str(e)}
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
 
 
 def logaut(email:str):
     try:
         conn, cur = connect_to_db()
         if conn is None or cur is None:
-            return {"error": "DB didn't connect"}
+            raise HTTPException(status_code=500, detail="DB connection failed")
         
         
-        cur.execute("SELECT 1 FROM users WHERE email = %s;", (email,))
+        cur.execute("SELECT is_logged_in FROM users WHERE email = %s;", (email,))
         result = cur.fetchone()
 
         if result is None:
-            return {"error": "Invalid email or password!"}
+            raise HTTPException(status_code=401, detail="Invalid email")
+        if result[0] is False:
+            raise HTTPException(status_code=409, detail="User already logged in")
 
         cur.execute(
                     """
@@ -183,23 +165,17 @@ def logaut(email:str):
                     """,
                     (email,)
                 )
-
         conn.commit()
         cur.close()
         conn.close()
         return {"user:", email, "was disconnected!"}
 
-
     except Exception as e:
-        print(f"Exception with updating balance: {e}")
-        return {"error": str(e)}
-        
-        
-        
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
 
-    except Exception as e:
-        print(f"Exception with updating balance: {e}")
-        return {"error": str(e)}
+
+
+
 app = FastAPI()
 
 
@@ -232,3 +208,6 @@ def update_balance_endpoint(email: str, amount: float):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+
