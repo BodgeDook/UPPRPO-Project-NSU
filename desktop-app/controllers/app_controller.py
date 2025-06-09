2635c692e7777b7c62f71620d8b49146d6412cc3
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2025-06-08 23:50:31 -0500

    Update app_controller.py

--- desktop-app/controllers/app_controller.py
+++ desktop-app/controllers/app_controller.py
@@ -1,6 +1,6 @@
 import os
 
-from PyQt5.QtWidgets import QFileDialog, QInputDialog
+from PyQt5.QtWidgets import QFileDialog, QInputDialog, QDialog, QVBoxLayout
 # from PyQt5.QtWidgets import QFileDialog, QDialog, QVBoxLayout
 # develop
 from PyQt5.QtCore import QStandardPaths
